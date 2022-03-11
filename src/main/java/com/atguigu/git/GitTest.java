367d1ddda5f9408a5c68d2ee7bb7f8abcd884dd1
Sparklight <Sparklight@qq.com>
2022-03-11 21:17:01 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @version 1.0
+ * @auther 石群
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
