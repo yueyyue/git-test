c2209c4a0d2cf54489e39a8eeac3301ca6c31467
Sparklight <Sparklight@qq.com>
2022-03-12 08:17:49 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,7 +6,13 @@ package com.atguigu.git;
  */
 public class GitTest {
 
+    /**
+     * //通过git建立版本控制后,文件是红色
+     * //添加暂存区后变成绿色, 提交本地库后变成正常颜色
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!"); //一旦代码修改后,会显示蓝色, 证明被追踪过
     }
 }
